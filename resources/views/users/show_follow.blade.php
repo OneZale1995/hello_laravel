abbc2e03855dda705cd45b421992cea58bf0fb54
OneZale1995 <442342634@qq.com>
2019-08-04 20:04:00 +0800

    关注用户动态流

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -3,6 +3,15 @@
 
 @section('content')
   <div class="offset-md-2 col-md-8">
+  <section class="user_info">
+      @include('shared._user_info', ['user' => Auth::user()])
+    </section>
+
+    <section class="stats mt-2">
+      @include('shared._stats', ['user' => Auth::user()])
+    </section>
+    <hr>
+
     <h2 class="mb-4 text-center">{{ $title }}</h2>
 
     <div class="list-group list-group-flush">
